6b6d620dadbd808a51ce776307cf16574de1faa1
ALambiasi <albertonandu@gmail.com>
2024-05-22 23:16:05 -0300

    footer almost done boys!!

---
+++ WebSolvers-master/js/scripts.js
@@ -0,0 +1,7 @@
+<script type="text/javascript">
+    function redirect()
+    {
+    var url = "https://www.instagram.com/pepeconstruye_construcciones/";
+    window.location(url);
+    }
+    </script>
